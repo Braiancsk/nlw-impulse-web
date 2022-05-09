c12a5230117d5f20e6c7c00453cb8ee88a3ec3d2
Braian Fellipe <74879806+Braiancsk@users.noreply.github.com>
2022-05-09 11:24:31 -0300

    Envio do server
    Envio dos arquivos finais do server

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+import { routes } from './routes'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT = 3333
+
+
+app.use(routes)
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`)
+})
